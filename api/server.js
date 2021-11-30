be509993c8e4af849eb77f069c813c4b6ef0f236
WaylayOvercast <timelyovercast@gmail.com>
2021-11-29 18:05:25 -0600

    finished work with all tests passing

--- api/server.js
+++ api/server.js
@@ -6,19 +6,56 @@ const Users = require('./users/model');
 server.use(express.json());
 
 
+server.put('/api/users/:id', async (req, res)=> {
+    try{
+        if (!await Users.findById(req.params.id)){
+            
+            res.status(404).json({
+                message: `The user with the specified ID does not exist, ID:${req.params.id}`,
+            })
+
+        }else if (!req.body.name || !req.body.bio) {
+
+            res.status(400).json({
+                message:`Please provide name and bio for the user, recieved name:${req.body.name} and bio:${req.body.bio}`
+            })
+
+        }else{
+
+            const updatedUser = await Users.update(req.params.id, req.body)
+            res.status(200).json({bio: updatedUser.bio, id: updatedUser.id, name: updatedUser.name})
+
+        }
+    }catch(err){
+        res.status(500).json({
+            message: `unspecified error updating user with ID of ${req.params.id}`,
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
 
 server.delete('/api/users/:id', async (req, res) => {
    
-   if (!await Users.findById(req.params.id)){
-        
-        res.status(404).json({
-            message: `The user with the specified ID does not exist, ID:${req.params.id}`,
-        })
+    try{
+        if (!await Users.findById(req.params.id)){
+                
+                res.status(404).json({
+                    message: `The user with the specified ID does not exist, ID:${req.params.id}`,
+                })
 
-   }else{
-        res.status(201).json(await Users.remove(req.params.id))
-   }
-})
+        }else{
+                res.status(200).json(await Users.remove(req.params.id))
+            }
+    }catch (err){
+        res.status(500).json({
+            message: `unspecified error while trying to delete user with valid ID of ${req.params.id}`,
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+});
 
 
 server.post('/api/users', (req, res) => {
